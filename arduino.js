32e0c04f5880022841c4382864f728cafa2124b7
Danzibob <danzibob@gmail.com>
2017-11-06 23:11:25 +0000

    Commented out serialy stuff

--- arduino.js
+++ arduino.js
@@ -1,27 +1,27 @@
-const SerialPort = require('serialport');
-const port = new SerialPort('/dev/ttyUSB0', {
-  baudRate: 9600
-})
+// const SerialPort = require('serialport');
+// const port = new SerialPort('/dev/ttyUSB0', {
+//   baudRate: 9600
+// })
 
-port.on('error', function (err) {
-  console.log('Error: ', err.message)
-})
+// port.on('error', function (err) {
+//   console.log('Error: ', err.message)
+// })
 
-port.on('readable', function () {
-  data += port.read()
-  console.log(data)
-})
+// port.on('readable', function () {
+//   data += port.read()
+//   console.log(data)
+// })
 
-port.on('data', function (data) {
-  console.log('Data:', data.toString('utf8'));
-});
+// port.on('data', function (data) {
+//   console.log('Data:', data.toString('utf8'));
+// });
 
 function sendCmd(message, online) {
   if (online) {
     console.log("sending: ", message);
-    port.write(message, function (err) {
-      return err || "Success!"
-    })
+    // port.write(message, function (err) {
+    //   return err || "Success!"
+    // })
   }
 }
 
