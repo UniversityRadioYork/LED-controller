97bc2d2276a087ef0074ec3ce5861e5be3cd8d95
Danzibob <danzibob@gmail.com>
2017-11-11 16:41:39 +0000

    Added verbosity to logs

--- arduino.js
+++ arduino.js
@@ -2,6 +2,8 @@ const SerialPort = require('serialport');
 const port = new SerialPort('COM3', {
   baudRate: 9600
 })
+var data = ""
+var need_validation = false;
 
 port.on('error', function (err) {
   console.log('Error: ', err.message)
@@ -12,10 +14,15 @@ port.on('readable', function () {
   if (data.indexOf("\n") > -1) {
     let datas = data.split("\n");
     data = datas[1];
-    let validation = datas[0];
-    if (validation != last_command) {
+    let chunk = datas[0];
+    console.log("Recieved:", chunk)
+    if (need_validation && chunk != last_command) {
       console.log("Command was garbled, resending...")
       sendCmd(last_command, true);
+    } else {
+      console.log("Command Validated, sending valid signal")
+      sendCmd("#", true);
+      need_validation = false;
     }
   }
 
@@ -31,6 +38,7 @@ async function sendCmd(message, online) {
     console.log("Sending: ", message);
     last_command = message;
     port.write(message, function (err) {
+      need_validation = true;
       return err || console.log("Sent");
     })
   }
